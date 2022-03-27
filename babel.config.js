159633ddce06fc73a662899e2b40f6d28f92a512
Marco Quintella <2om.marco.quintella@gmail.com>
2022-03-27 11:28:31 -0300

    Removed Console on Production

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+const productionPlugins = []
+
+if (process.env.NODE_ENV === 'production') productionPlugins.push('transform-remove-console')
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ...productionPlugins
   ]
 }
